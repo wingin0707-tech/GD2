767bddedb117f62c6d71f6d766886811cad3163d
wingin0707-tech <wingin0707@gmail.com>
2026-02-26 20:32:45 -0500

    sound map
    changes edit

---
+++ Soundmap/soundmap.js
@@ -0,0 +1,9 @@
+function showAbout() {
+    document.getElementById("home-section").style.display = "none";
+    document.getElementById("about-section").style.display = "block";
+}
+
+function showHome() {
+    document.getElementById("home-section").style.display = "block";
+    document.getElementById("about-section").style.display = "none";
+}
