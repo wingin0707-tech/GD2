ae2fdc5d2e1ad4f8f8c6809ff82150897bd2d8e0
wingin0707-tech <wingin0707@gmail.com>
2026-02-26 20:40:44 -0500

    menu button

--- Soundmap/soundmap.js
+++ Soundmap/soundmap.js
@@ -1,9 +1,20 @@
-function showAbout() {
-    document.getElementById("home-section").style.display = "none";
-    document.getElementById("about-section").style.display = "block";
-}
 
-function showHome() {
-    document.getElementById("home-section").style.display = "block";
-    document.getElementById("about-section").style.display = "none";
-}
+    const menuButton = document.getElementById('menuButton');
+        const menuContainer = document.getElementById('menuContainer');
+
+        menuButton.addEventListener('click', () => {
+            // Toggle spinning
+            menuButton.classList.toggle('spin');
+            // Toggle menu visibility
+            menuContainer.style.display = menuContainer.style.display === 'block' ? 'none' : 'block';
+        });
+
+        function showAbout() {
+            document.getElementById("home-section").style.display = "none";
+            document.getElementById("about-section").style.display = "block";
+        }
+        
+        function showHome() {
+            document.getElementById("home-section").style.display = "block";
+            document.getElementById("about-section").style.display = "none";
+        }
