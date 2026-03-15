657812f0ecbb146734fa6b8219b10cb35cb2c912
wingin0707-tech <wingin0707@gmail.com>
2026-03-15 15:56:06 -0400

    changes

--- Soundmap/soundmap.js
+++ Soundmap/soundmap.js
@@ -1,5 +1,5 @@
-
 document.addEventListener("DOMContentLoaded", () => {
+
     const menuButton = document.getElementById("menuButton");
     const menuContainer = document.getElementById("menuContainer");
 
@@ -7,21 +7,65 @@ document.addEventListener("DOMContentLoaded", () => {
         menuContainer.classList.toggle("show");
         menuButton.classList.toggle("spin");
     });
+
 });
 
-const aboutMenuButton = document.getElementById("aboutMenuButton");
-const aboutMenuContainer = document.getElementById("aboutMenuContainer");
-
-aboutMenuButton.addEventListener("click", () => {
-    aboutMenuContainer.classList.toggle("show");
-});
-
-function showAbout() {
+    function showAbout() {
     document.getElementById("home-section").style.display = "none";
     document.getElementById("about-section").style.display = "block";
 }
 
-function showHome() {
+        function showHome() {
     document.getElementById("home-section").style.display = "block";
     document.getElementById("about-section").style.display = "none";
+}
+
+    function initMap() {
+
+const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 43.8561, lng: -79.3370 },
+    zoom: 12,
+
+    styles: [
+        {
+            featureType: "all",
+            elementType: "all",
+            stylers: [
+                { visibility: "on" },
+                { color: "#ede6d2" },
+                { saturation: -33 },
+                { lightness: 14 }
+            ]
+        },
+        {
+            featureType: "administrative",
+            elementType: "geometry",
+            stylers: [{ visibility: "off" }]
+        },
+        {
+            featureType: "poi",
+            elementType: "all",
+            stylers: [{ visibility: "off" }]
+        },
+        {
+            featureType: "transit",
+            elementType: "all",
+            stylers: [{ visibility: "off" }]
+        },
+        {
+            featureType: "water",
+            elementType: "all",
+            stylers: [{ visibility: "off" }]
+        },
+        {
+            featureType: "road.highway",
+            elementType: "geometry.stroke",
+            stylers: [
+                { visibility: "on" },
+                { color: "#0d1f4e" }
+            ]
+        }
+    ]
+});
+
 }
