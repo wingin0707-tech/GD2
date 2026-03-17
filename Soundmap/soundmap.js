985edd8dc04f05974184e0ee145f2d3a8d78ff0f
wingin0707-tech <wingin0707@gmail.com>
2026-03-17 17:41:38 -0400

    changes
    hehehhe

--- Soundmap/soundmap.js
+++ Soundmap/soundmap.js
@@ -1,3 +1,464 @@
+const map = L.map('map').setView([43.8561, -79.3370], 13);
+
+
+// MapLibreGL layer using OpenStreetMap raster tiles
+
+L.maplibreGL({
+  style: {
+  "version": 8,
+  "name": "Toner",
+  "metadata": {
+    "mapbox:autocomposite": false,
+    "mapbox:type": "template",
+    "openmaptiles:mapbox:owner": "openmaptiles",
+    "openmaptiles:mapbox:source:url": "mapbox://openmaptiles.4qljc88t",
+    "openmaptiles:version": "3.x",
+    "maputnik:renderer": "mlgljs"
+  },
+  "center": [
+    -122.41877447993727,
+    37.7977350127602
+  ],
+  "zoom": 10.426085190067841,
+  "bearing": 0,
+  "pitch": 0,
+  "sources": {
+    "openmaptiles": {
+      "type": "vector",
+      "url": "https://api.maptiler.com/tiles/v3-openmaptiles/tiles.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL"
+    }
+  },
+  "glyphs": "https://api.maptiler.com/fonts/{fontstack}/{range}.pbf?key=get_your_own_OpIi9ZULNHzrESv6T2vL",
+  "layers": [
+    {
+      "id": "background",
+      "paint": {
+        "background-color": "rgba(255, 255, 255, 1)"
+      },
+      "type": "background",
+      "filter": ["all"]
+    },
+    {
+      "filter": [
+        "==",
+        "class",
+        "grass"
+      ],
+      "id": "landcover_grass_fill",
+      "metadata": {
+        "mapbox:group": "1444849388993.3071"
+      },
+      "minzoom": 10,
+      "paint": {
+        "fill-antialias": true,
+        "fill-color": {
+          "stops": [
+            [6, "#2044e9"],
+            [10, "#2044e9"]
+          ],
+          "type": "exponential"
+        },
+        "fill-opacity": 1,
+        "fill-outline-color": "rgba(255, 32, 32, 1)"
+      },
+      "source": "openmaptiles",
+      "source-layer": "landcover",
+      "type": "fill",
+      "layout": {
+        "visibility": "visible"
+      }
+    },
+    {
+      "filter": [
+        "==",
+        "class",
+        "grass"
+      ],
+      "id": "landcover_grass_pattern",
+      "metadata": {
+        "mapbox:group": "1444849388993.3071"
+      },
+      "minzoom": 10,
+      "paint": {
+        "fill-antialias": true,
+        "fill-opacity": {
+          "property": "",
+          "type": "categorical",
+          "stops": [
+            [
+              {"zoom": 10, "value": 0},
+              0.75
+            ],
+            [
+              {"zoom": 14, "value": 0},
+              1
+            ]
+          ]
+        },
+        "fill-outline-color": "rgba(255, 0, 0, 1)",
+        "fill-pattern": "dash-t",
+        "fill-color": "rgba(248, 14, 14, 1)"
+      },
+      "source": "openmaptiles",
+      "source-layer": "landcover",
+      "type": "fill",
+      "layout": {
+        "visibility": "visible"
+      }
+    },
+    {
+      "filter": ["==", "class", "wood"],
+      "id": "landcover_wood_fill",
+      "layout": {
+        "visibility": "visible"
+      },
+      "minzoom": 10,
+      "paint": {
+        "fill-antialias": true,
+        "fill-color": "rgba(255, 0, 0, 1)",
+        "fill-opacity": {
+          "property": "",
+          "type": "exponential",
+          "stops": [
+            [
+              {"zoom": 6, "value": 0},
+              1
+            ],
+            [
+              {"zoom": 10, "value": 0},
+              1
+            ]
+          ]
+        }
+      },
+      "source": "openmaptiles",
+      "source-layer": "landcover",
+      "type": "fill"
+    },
+    {
+      "filter": ["==", "class", "wood"],
+      "id": "landcover_wood_pattern",
+      "layout": {
+        "visibility": "visible"
+      },
+      "minzoom": 7,
+      "paint": {
+        "fill-antialias": true,
+        "fill-color": "rgba(228, 42, 42, 1)",
+        "fill-opacity": 1,
+        "fill-pattern": "dots-t"
+      },
+      "source": "openmaptiles",
+      "source-layer": "landcover",
+      "type": "fill"
+    },
+    {
+      "filter": [
+        "all",
+        ["!=", "brunnel", "tunnel"],
+        ["==", "$type", "Polygon"],
+        ["!=", "intermittent", 1]
+      ],
+      "id": "water",
+      "layout": {
+        "visibility": "visible"
+      },
+      "paint": {
+        "fill-antialias": true,
+        "fill-color": "rgba(155, 216, 195, 1)",
+        "fill-translate-anchor": "viewport"
+      },
+      "source": "openmaptiles",
+      "source-layer": "water",
+      "type": "fill"
+    },
+    {
+      "filter": [
+        "all",
+        ["!=", "class", "river"]
+      ],
+      "id": "waterway",
+      "minzoom": 12,
+      "source": "openmaptiles",
+      "source-layer": "waterway",
+      "type": "line",
+      "paint": {
+        "line-color": "rgba(255, 0, 103, 1)"
+      },
+      "maxzoom": 24,
+      "layout": {
+        "line-cap": "butt",
+        "line-join": "miter",
+        "visibility": "visible"
+      }
+    },
+    {
+      "filter": [
+        "all",
+        ["==", "class", "rail"]
+      ],
+      "id": "rail",
+      "layout": {
+        "visibility": "visible"
+      },
+      "minzoom": 13,
+      "paint": {
+        "line-color": {
+          "stops": [
+            [
+              13,
+              "rgba(216, 216, 216, 1)"
+            ],
+            [17, "rgba(38, 38, 38, 1)"]
+          ]
+        },
+        "line-width": {
+          "stops": [[6, 1], [10, 1]],
+          "type": "exponential"
+        }
+      },
+      "source": "openmaptiles",
+      "source-layer": "transportation",
+      "type": "line"
+    },
+    {
+      "filter": [
+        "all",
+        ["==", "class", "rail"]
+      ],
+      "id": "rail_hatch",
+      "layout": {
+        "visibility": "visible"
+      },
+      "minzoom": 17,
+      "paint": {
+        "line-color": "rgba(38, 38, 38, 1)",
+        "line-dasharray": {
+          "stops": [
+            [15, [0.2, 0.8]],
+            [17, [0.2, 1]]
+          ]
+        },
+        "line-width": {
+          "stops": [[15, 2], [17, 6]]
+        }
+      },
+      "source": "openmaptiles",
+      "source-layer": "transportation",
+      "type": "line"
+    },
+    {
+      "filter": [
+        "all",
+        [
+          "in",
+          "class",
+          "secondary",
+          "tertiary",
+          "minor",
+          "service"
+        ]
+      ],
+      "id": "road_secondary",
+      "layout": {
+        "line-cap": "square",
+        "line-join": "miter",
+        "visibility": "visible"
+      },
+      "metadata": {},
+      "minzoom": 13,
+      "paint": {
+        "line-color": "rgba(239, 10, 10, 0.99)",
+        "line-width": 1,
+        "line-translate-anchor": "viewport",
+        "line-blur": 1.5
+      },
+      "source": "openmaptiles",
+      "source-layer": "transportation",
+      "type": "line",
+      "maxzoom": 19
+    },
+    {
+      "filter": [
+        "all",
+        ["==", "$type", "Point"],
+        ["==", "class", "city"]
+      ],
+      "id": "place_label_city",
+      "layout": {
+        "icon-anchor": "center",
+        "text-field": "{name:latin}",
+        "text-font": {
+          "stops": [
+            [4, ["Nunito Regular"]],
+            [7, ["Nunito Bold"]]
+          ]
+        },
+        "text-max-width": 10,
+        "text-size": {
+          "stops": [
+            [4, 14],
+            [7, 15],
+            [8, 19],
+            [16, 22]
+          ]
+        },
+        "visibility": "visible"
+      },
+      "maxzoom": 16,
+      "minzoom": 4,
+      "paint": {
+        "icon-translate": [1, 11],
+        "text-color": "rgba(0, 0, 0, 1)",
+        "text-halo-blur": 0,
+        "text-halo-color": "rgba(255, 255, 255, 1)",
+        "text-halo-width": 2
+      },
+      "source": "openmaptiles",
+      "source-layer": "place",
+      "type": "symbol"
+    },
+    {
+      "filter": [
+        "all",
+        ["==", "$type", "Point"],
+        ["==", "class", "town"]
+      ],
+      "id": "place_label_town",
+      "layout": {
+        "icon-anchor": "center",
+        "text-field": "{name:latin}",
+        "text-font": {
+          "stops": [
+            [8, ["Nunito Regular"]],
+            [14, ["Nunito Bold"]]
+          ]
+        },
+        "text-max-width": 10,
+        "text-size": {
+          "stops": [[8, 15], [16, 18]]
+        },
+        "visibility": "visible"
+      },
+      "maxzoom": 16,
+      "minzoom": 10,
+      "paint": {
+        "icon-translate": [1, 11],
+        "text-color": "rgba(0, 0, 0, 1)",
+        "text-halo-blur": 0,
+        "text-halo-color": "rgba(255, 255, 255, 1)",
+        "text-halo-width": 2
+      },
+      "source": "openmaptiles",
+      "source-layer": "place",
+      "type": "symbol"
+    },
+    {
+      "filter": [
+        "all",
+        ["==", "class", "state"]
+      ],
+      "id": "place_state-label",
+      "layout": {
+        "text-field": "{name:latin} {name:nonlatin}",
+        "text-font": [
+          "Nunito Semi Bold"
+        ],
+        "text-max-width": 10,
+        "text-size": 13,
+        "text-transform": "uppercase",
+        "visibility": "visible"
+      },
+      "metadata": {
+        "mapbox:group": "a14c9607bc7954ba1df7205bf660433f"
+      },
+      "minzoom": 4,
+      "paint": {
+        "text-color": "rgba(0, 0, 0, 1)"
+      },
+      "source": "openmaptiles",
+      "source-layer": "place",
+      "type": "symbol"
+    },
+    {
+      "filter": [
+        "==",
+        "class",
+        "country"
+      ],
+      "id": "place_label_country",
+      "layout": {
+        "text-allow-overlap": false,
+        "text-field": "{name:latin}",
+        "text-font": {
+          "stops": [
+            [3, ["Nunito Regular"]],
+            [4, ["Nunito Bold"]]
+          ]
+        },
+        "text-ignore-placement": false,
+        "text-max-width": 10,
+        "text-padding": 2,
+        "text-pitch-alignment": "map",
+        "text-size": {
+          "stops": [
+            [3, 14],
+            [4, 16],
+            [5, 21]
+          ]
+        },
+        "text-transform": "none",
+        "visibility": "visible"
+      },
+      "minzoom": 2,
+      "paint": {
+        "text-color": "rgba(0, 0, 0, 1)",
+        "text-halo-color": "#fff",
+        "text-halo-width": 2
+      },
+      "source": "openmaptiles",
+      "source-layer": "place",
+      "type": "symbol"
+    },
+    {
+      "filter": [
+        "==",
+        "class",
+        "continent"
+      ],
+      "id": "place_label_continent",
+      "layout": {
+        "text-field": "{name:latin}",
+        "text-font": [
+          "Nunito Extra Bold"
+        ],
+        "text-line-height": 1.1,
+        "text-max-width": 10,
+        "text-size": {
+          "stops": [[3, 18], [4, 24]]
+        },
+        "visibility": "visible"
+      },
+      "maxzoom": 2,
+      "minzoom": 1,
+      "paint": {
+        "text-color": "rgba(0, 0, 0, 1)",
+        "text-halo-color": "#fff",
+        "text-halo-width": 2
+      },
+      "source": "openmaptiles",
+      "source-layer": "place",
+      "type": "symbol"
+    }
+  ],
+  "sprite": "https://openmaptiles.github.io/maptiler-toner-gl-style/sprite",
+  "id": "c4268e48-fac9-4478-8120-201224fbd4d8"
+}
+}).addTo(map);
+
+
+
+
 const menuButton = document.getElementById("menuButton");
 const menuContainer = document.getElementById("menuContainer");
 
@@ -23,15 +484,7 @@ startButton.addEventListener("click", () => {
   introPopup.style.display = "none";
 });
 
-// INITIALIZE MAP
-const map = L.map('map').setView([43.8561, -79.3370], 12);
 
-// ONLY ONE TILE LAYER (Dark tiles, which we will invert to white/purple in CSS)
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; OpenStreetMap &copy; CARTO',
-  subdomains: 'abcd',
-  maxZoom: 20
-}).addTo(map);
 
 const soundLocations = [
   { title: "Edward Jeffreys Ave", description: "Edward Jeffreys avenue GO train station", lat: 43.89411, lng: -79.27344, time: "16 Feb, 2026 02:55pm", audio: "https://image2url.com/r2/default/audio/1771616956404-94393f99-3144-4b5c-95fd-891869b3fdf8.m4a" },
