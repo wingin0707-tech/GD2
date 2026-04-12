8d563ac7c6f4f260e83412f50d5c6c9e0ae22860
wingin0707-tech <wingin0707@gmail.com>
2026-04-12 17:41:57 -0400

    changes

--- Soundmap/soundmap.js
+++ Soundmap/soundmap.js
@@ -443,6 +443,11 @@ L.maplibreGL({
   }
 }).addTo(map);
 
+// Add this right after: const map = L.map('map').setView(...);
+map.createPane('markerOverlay');
+map.getPane('markerOverlay').style.zIndex = 650; // High enough to beat the map layer
+map.getPane('markerOverlay').style.pointerEvents = 'none'; // So you can still pan the map
+
 
 const cursor = document.createElement("div");
 cursor.classList.add("custom-cursor");
@@ -538,29 +543,54 @@ if (startBtn) {
     }, 1200); 
   });
 }
+const crossIcon = L.divIcon({
+  className: 'custom-cross-icon',
+  html: '<div class="cross-marker"></div>', 
+  iconSize: [20, 20],
+  iconAnchor: [10, 10]
+});
 
+
+const myCustomIcon = L.divIcon({
+  className: 'custom-cross-icon', // The class name for your CSS
+  html: '<div class="marker-dot"></div>', // The actual HTML element
+  iconSize: [20, 20], // The logical size of the icon
+  iconAnchor: [10, 10] // The "tip" of the icon (center it)
+});
 // 5. Markers & Data
-const soundLocations = [
-  { title: "Edward Jeffreys Ave", description: "Edward Jeffreys avenue GO train station", lat: 43.89411, lng: -79.27344, time: "16 Feb, 2026 02:55pm", audio: "https://image2url.com/r2/default/audio/1771616956404-94393f99-3144-4b5c-95fd-891869b3fdf8.m4a", image: "Markham-Village-Train-Station-3.jpg" },
-  { title: "Scott Brown St", description: "25 Scott Brown St", lat: 43.89945, lng: -79.27401, time: "17 Feb, 2026 5:30pm", audio: "https://image2url.com/r2/default/audio/1771617515406-ea055bbd-6cb8-460f-b1be-d6a683aae14a.m4a" },
-  { title: "Edward Jeffreys GO Bus Station", description: "Markham Rd at Edward Jeffreys Ave", lat: 43.896358, lng: -79.265278, time: "16 Feb, 2026 10:20pm", audio: "https://image2url.com/r2/default/audio/1771617711520-42b4daf9-bea6-409f-98a2-931ec4d6aa53.m4a" },
-  { title: "Bur Oak Ave", description: "Bur Oak Ave", lat: 43.8929, lng: -79.3022, time: "17 Feb, 2026 4:00pm", audio: "https://image2url.com/r2/default/audio/1771617858226-0335278e-1a9a-4509-86d6-d80ff5f8aca6.m4a" },
-  { title: "South Unionville Ave", description: "Highway 407", lat: 43.8605, lng: -79.2833, time: "16 Feb, 2026 12:00pm", audio: "https://image2url.com/r2/default/audio/1771618112948-02075d34-09c1-4200-8bc5-6e1b130472f2.m4a" },
-  { title: "Markham Auto Spa", description: "New Kennedy Mall", lat: 43.85934, lng: -79.3047, time: "17 Feb, 2026 5:47pm", audio: "https://image2url.com/r2/default/audio/1771618383533-1b1844c7-6186-4d66-a64e-d8fb2e0b8eff.m4a" },
-  { title: "Edward Jeffreys Ave", description: "Inside GO Train", lat: 43.89412, lng: -79.27343, time: "16 Feb, 2026 02:45pm", audio: "https://image2url.com/r2/default/audio/1771618633399-02792719-3ee0-4572-b59a-75379656f70b.m4a" },
-  { title: "Kennedy Rd", description: "T&T Meat Market", lat: 43.85934, lng: -79.3047, time: "16 Feb, 2026 4:32pm", audio: "https://image2url.com/r2/default/audio/1771618722900-06fc915f-343b-4b38-85a1-6ec0bc6ed6b0.m4a" },
-  { title: "Markham GO Bus", description: "Highway 407", lat: 43.896358, lng: -79.265278, time: "16 Feb, 2026 10:46pm", audio: "https://image2url.com/r2/default/audio/1771619947868-cef868a6-5059-4f3d-bddb-3028eb21ac17.m4a" },
-  { title: "Markham Plaza", description: "Train", lat: 43.8763, lng: -79.2575, time: "17 Feb, 2026 4:00pm", audio: "https://image2url.com/r2/default/audio/1772144198078-d1de5c85-3734-4222-abdb-8915acf204de.m4a" },
-  { title: "Kennedy Rd", description: "Kennedy Rd", lat: 43.874527, lng: -79.286243, time: "18 Feb, 2026 8:30pm", audio: "https://image2url.com/r2/default/audio/1772144461963-b6596e1b-7a63-40de-9dc0-771281e02950.m4a" },
-  { title: "Mercedes-Benz Markham", description: "Mercedes-Benz Markham", lat: 43.8577525, lng: -79.3043853, time: "19 Feb, 2026 5:02pm", audio: "https://image2url.com/r2/default/audio/1772144515177-ef239853-490c-4881-9f99-78d92d8c7fdc.m4a" },
-  { title: "Milliken Meadows Dr GO Train", description: "Milliken Meadows Dr GO Train", lat: 43.82319, lng: -79.30167, time: "20 Feb, 2026 4:33pm", audio: "https://image2url.com/r2/default/audio/1772144564808-71c77456-09c8-4500-814d-5019b67bffd2.m4a" },
-  { title: "Day & Night King Crab", description: "Day and Night Crab Kitchen", lat: 43.82392, lng: -79.30074, time: "21 Feb, 2026 7:16pm", audio: "https://image2url.com/r2/default/audio/1772144653984-18449777-4c39-44e2-84f8-30ac16ffbf44.m4a" }
-];
+    const soundLocations = [
+   { title: "Edward Jeffreys Ave", description: "Edward Jeffreys avenue GO train station", lat: 43.89411, lng: -79.27344, time: "16 Feb, 2026 02:55pm", audio: "https://image2url.com/r2/default/audio/1771616956404-94393f99-3144-4b5c-95fd-891869b3fdf8.m4a", image: "Markham-Village-Train-Station-3.jpg" },
+   { title: "Scott Brown St", description: "25 Scott Brown St", lat: 43.89945, lng: -79.27401, time: "17 Feb, 2026 5:30pm", audio: "https://image2url.com/r2/default/audio/1771617515406-ea055bbd-6cb8-460f-b1be-d6a683aae14a.m4a" },
+    { title: "Edward Jeffreys GO Bus Station", description: "Markham Rd at Edward Jeffreys Ave", lat: 43.896358, lng: -79.265278, time: "16 Feb, 2026 10:20pm", audio: "https://image2url.com/r2/default/audio/1771617711520-42b4daf9-bea6-409f-98a2-931ec4d6aa53.m4a" },
+    { title: "Bur Oak Ave", description: "Bur Oak Ave", lat: 43.8929, lng: -79.3022, time: "17 Feb, 2026 4:00pm", audio: "https://image2url.com/r2/default/audio/1771617858226-0335278e-1a9a-4509-86d6-d80ff5f8aca6.m4a" },
+    { title: "South Unionville Ave", description: "Highway 407", lat: 43.8605, lng: -79.2833, time: "16 Feb, 2026 12:00pm", audio: "https://image2url.com/r2/default/audio/1771618112948-02075d34-09c1-4200-8bc5-6e1b130472f2.m4a" },
+      { title: "Markham Auto Spa", description: "New Kennedy Mall", lat: 43.85934, lng: -79.3047, time: "17 Feb, 2026 5:47pm", audio: "https://image2url.com/r2/default/audio/1771618383533-1b1844c7-6186-4d66-a64e-d8fb2e0b8eff.m4a" },
+    { title: "Edward Jeffreys Ave", description: "Inside GO Train", lat: 43.89412, lng: -79.27343, time: "16 Feb, 2026 02:45pm", audio: "https://image2url.com/r2/default/audio/1771618633399-02792719-3ee0-4572-b59a-75379656f70b.m4a" },
+    { title: "Kennedy Rd", description: "T&T Meat Market", lat: 43.85934, lng:-79.3047, time: "16 Feb, 2026 4:32pm", audio: "https://image2url.com/r2/default/audio/1771618722900-06fc915f-343b-4b38-85a1-6ec0bc6ed6b0.m4a" },
+    { title: "Markham GO Bus", description: "Highway 407", lat: 43.896358, lng: -79.265278, time: "16 Feb, 2026 10:46pm", audio: "https://image2url.com/r2/default/audio/1771619947868-cef868a6-5059-4f3d-bddb-3028eb21ac17.m4a" },
+    { title: "Markham Plaza", description: "Train", lat: 43.8763, lng: -79.2575, time: "17 Feb, 2026 4:00pm", audio: "https://image2url.com/r2/default/audio/1772144198078-d1de5c85-3734-4222-abdb-8915acf204de.m4a" },
+    { title: "Kennedy Rd", description: "Kennedy Rd", lat: 43.874527, lng: -79.286243, time: "18 Feb, 2026 8:30pm", audio: "https://image2url.com/r2/default/audio/1772144461963-b6596e1b-7a63-40de-9dc0-771281e02950.m4a" },
+    { title: "Mercedes-Benz Markham", description: "Mercedes-Benz Markham", lat: 43.8577525, lng: -79.3043853, time: "19 Feb, 2026 5:02pm", audio: "https://image2url.com/r2/default/audio/1772144515177-ef239853-490c-4881-9f99-78d92d8c7fdc.m4a" },
+    { title: "Milliken Meadows Dr GO Train", description: "Milliken Meadows Dr GO Train", lat: 43.82319, lng: -79.30167, time: "20 Feb, 2026 4:33pm", audio: "https://image2url.com/r2/default/audio/1772144564808-71c77456-09c8-4500-814d-5019b67bffd2.m4a" },
+    { title: "Day & Night King Crab", description: "Day and Night Crab Kitchen", lat: 43.82392, lng: -79.30074, time: "21 Feb, 2026 7:16pm", audio: "https://image2url.com/r2/default/audio/1772144653984-18449777-4c39-44e2-84f8-30ac16ffbf44.m4a" }
+    ];
+    soundLocations.forEach((location) => {
+      const marker = L.marker([location.lat, location.lng], {
+        icon: crossIcon,
+        pane: 'markerOverlay', // THIS IS THE KEY!
+        zIndexOffset: 2000 
+      }).addTo(map);
+    
+      marker.bindPopup(`
+        <div class="popup-content" style="color:black;">
+          <h3 style="color:#ff0000; margin:0;">${location.title}</h3>
+          <p>${location.description}</p>
+          <audio controls src="${location.audio}" style="width:100%;"></audio>
+        </div>
+      `);
 
-const marker = L.marker([location.lat, location.lng], {
-  icon: crossIcon,
-  zIndexOffset: 2000 // Force markers to stay on top of the vector map
-}).addTo(markerGroup);
+      
+    });
 
 // Optimized Intro Button Logic
 if (startBtn) {
